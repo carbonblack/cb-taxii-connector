4f2315b93901aad406f6968918ffefce348dd460
Benjamin Johnson <bjohnson@bit9.com>
2015-07-03 07:23:43 -0500

    Updated licensing information.

--- src/cbtaxii/util.py
+++ src/cbtaxii/util.py
@@ -1,3 +1,27 @@
+#!/usr/bin/env python
+#
+#The MIT License (MIT)
+#
+# Copyright (c) 2015 Bit9 + Carbon Black
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
 
 import sys
 import string
@@ -7,13 +31,6 @@ import unicodedata
 from datetime import timedelta, tzinfo
 from logging.handlers import RotatingFileHandler
 
-
-def cleanup_string(filename):
-#    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
-    valid_chars = "%s%s" % (string.ascii_letters, string.digits)
-    newname = unicodedata.normalize('NFKD', unicode(filename)).encode('ASCII', 'ignore')
-    s = ''.join(c for c in newname if c in valid_chars)
-    return s.lower()
 
 ZERO = timedelta(0)
 HOUR = timedelta(hours=1)
@@ -30,6 +47,12 @@ class UTC(tzinfo):
         return ZERO
 
 TZ_UTC = UTC()
+
+def cleanup_string(filename):
+    valid_chars = "%s%s" % (string.ascii_letters, string.digits)
+    newname = unicodedata.normalize('NFKD', unicode(filename)).encode('ASCII', 'ignore')
+    s = ''.join(c for c in newname if c in valid_chars)
+    return s.lower()
 
 
 def create_stdout_log(name, level):
